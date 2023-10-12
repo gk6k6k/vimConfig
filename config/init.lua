7ea65b63764f5d462846616b66a97e9b6cb319df
Adam Krupski <adam.krupski@grassvalley.com>
2023-10-12 15:52:57 +0200

    Add switch source keymap

--- config/init.lua
+++ config/init.lua
@@ -256,6 +256,11 @@ end, { desc = '[/] Fuzzily search in current buffer]' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>ss', function() 
+  require('telescope.builtin').find_files({
+    default_text=vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t:r")
+  }) 
+end, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
