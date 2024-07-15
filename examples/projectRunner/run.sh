616387df229ca07e4845579fc3ea50a1c33f66d4
Adam Krupski <adam.krupski@grassvalley.com>
2024-07-15 15:37:48 +0200

    upgrade runner

--- examples/projectRunner/run.sh
+++ examples/projectRunner/run.sh
@@ -6,9 +6,7 @@ if [ ! -f /.dockerenv ]; then
     if [ -z "$(docker images -q ${md5} 2> /dev/null)" ]; then
         docker build -t ${md5} .
     fi
-    docker run -it -v $(pwd):$(pwd) -w $(pwd) -p 3000:3000 -p 3001:3001 ${md5} /bin/bash
+    docker run -it -v $(pwd):$(pwd) -w $(pwd) ${md5} /bin/bash
 else
-    #build backend
-    echo "cmake -S ./backend/ -B /tmp/build -DCMAKE_TOOLCHAIN_FILE=/tmp/toolchain/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release"
-    echo "cmake --build /tmp/build/ -j"
+    if [ ! -f /tmp/build/Makefile ]; then cmake -S . -B /tmp/build; fi; cmake --build /tmp/build/ -j
 fi
