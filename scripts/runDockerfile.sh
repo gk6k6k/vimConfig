792a8e4940f213759f1b1d33305d78c215dfc69b
Adam Krupski <adam.krupski@grassvalley.com>
2025-07-08 15:38:18 +0200

    Docer run same container if image exists

--- scripts/runDockerfile.sh
+++ scripts/runDockerfile.sh
@@ -22,10 +22,30 @@ fi
 
 DOCKER_IMAGE_NAME=("autoimagename_"$(md5sum ${PATH_DOCKERFILE}))
 
+# https://stackoverflow.com/questions/21928691/how-to-continue-a-docker-container-which-has-exited
+# docker run --name UserName_date_imageNAme https://stackoverflow.com/questions/57695744/how-to-name-a-container-with-docker-run
+
 if [ ! -f /.dockerenv ]; then
     if [ -z "$(docker images -q ${DOCKER_IMAGE_NAME} 2> /dev/null)" ]; then
         docker build -t ${DOCKER_IMAGE_NAME} -f "${PATH_DOCKERFILE}" .
     fi
     echo "runing iamge ${DOCKER_IMAGE_NAME} ${ARGS_DOCKER}"
-    docker run -it -v $(pwd):$(pwd) -w $(pwd) ${ARGS_DOCKER} ${DOCKER_IMAGE_NAME} /bin/bash
+    DOCKER_CONTAINER_NAME="${USER}_${DOCKER_IMAGE_NAME}"
+
+    if docker inspect "${DOCKER_CONTAINER_NAME}" > /dev/null 2>&1; then
+        echo "${DOCKER_CONTAINER_NAME} exists."
+
+        if $(docker inspect -f '{{.State.Status}}' "${DOCKER_CONTAINER_NAME}" | grep -q "running"); then
+            echo "${DOCKER_CONTAINER_NAME} running."
+
+            docker attach "${DOCKER_CONTAINER_NAME}"
+        else
+            echo "${DOCKER_CONTAINER_NAME} not running."
+
+            docker start "${DOCKER_CONTAINER_NAME}"
+            docker attach "${DOCKER_CONTAINER_NAME}"
+        fi
+    else
+        docker run -it --name ${DOCKER_CONTAINER_NAME} -v $(pwd):$(pwd) -w $(pwd) ${ARGS_DOCKER} ${DOCKER_IMAGE_NAME} /bin/bash
+    fi
 fi
